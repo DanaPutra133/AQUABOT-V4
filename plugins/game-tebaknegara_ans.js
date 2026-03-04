61cddf1bf3efde40bf7f8119cb91549810a710bd
danaputra133 <danaputra375@gmail.com>
2026-03-05 05:20:21 +0700

    update game, fix fitur game exp -> money

--- plugins/game-tebaknegara_ans.js
+++ plugins/game-tebaknegara_ans.js
@@ -7,18 +7,18 @@
         let id = m.chat
         if (!m.quoted) return !0
 
-        this.tbkata = this.tbkata ? this.tbkata : {}
-        if (!(id in this.tbkata)) return !0
-        if (m.quoted.id !== this.tbkata[id][0].key.id) return !0
-        let json = this.tbkata[id][1]
-        let jawaban = json.jawaban.toLowerCase().trim()
+         this.tebaknegara =  this.tebaknegara ?  this.tebaknegara : {}
+        if (!(id in  this.tebaknegara)) return !0
+        if (m.quoted.id !==  this.tebaknegara[id][0].key.id) return !0
+        let json =  this.tebaknegara[id][1]
+        let jawaban = json.jawaban.toLowerCase().trim();
         let teksUser = (m.text || '').toLowerCase().trim()
         if (!teksUser) return !0
         if (teksUser === jawaban) {
-            global.db.data.users[m.sender].money += this.tbkata[id][2]
-            m.reply(`*Benar!*\n+${this.tbkata[id][2]} Kredit sosial`)
-            clearTimeout(this.tbkata[id][3])
-            delete this.tbkata[id]
+            global.db.data.users[m.sender].money +=  this.tebaknegara[id][2]
+            m.reply(`*Benar!*\n+${ this.tebaknegara[id][2]} Kredit sosial`)
+            clearTimeout( this.tebaknegara[id][3])
+            delete  this.tebaknegara[id]
         } 
         else if (similarity(teksUser, jawaban) >= threshold) {
             m.reply(`*Dikit Lagi!*`)
