fc95f2524db51e8438dc8800c3cccd526f602f3a
danaputra133 <danaputra375@gmail.com>
2026-03-10 05:23:30 +0700

    waifu

--- plugins/random_waifu.js
+++ plugins/random_waifu.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const { getBuffer } = require('../lib/myfunc');
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     try {
@@ -7,9 +8,11 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         const apiUrl = `https://api.danafxc.my.id/api/proxy/pict/gambar?q=waifu&apikey=${dana}`;
         const response = await axios.get(apiUrl);
         const jsonData = response.data;
+
         if (jsonData && jsonData.status && jsonData.urls && jsonData.urls.length > 0) {
             const imageUrl = jsonData.urls[0];
-                        conn.sendMessage(m.chat, imageUrl, 'random.jpg', `Ini gambar random *${kategori}* untukmu!`, m);
+            const buffer = await getBuffer(imageUrl);
+            await conn.sendFile(m.chat, buffer, 'random.jpg', 'Ini gambar random waifu untukmu!', m);
         } else {
             throw new Error('API tidak mengembalikan URL gambar yang valid.');
         }
