c61592ce6a0aa13ce0955bb2d560b5696e1a7dd7
danaputra133 <danaputra375@gmail.com>
2026-03-10 05:02:09 +0700

    random pict to aqua API,

---
+++ plugins/random_profilepict.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    try {
+        m.reply(`⏳ Sedang mencari gambar random`);
+        
+        const apiUrl = `https://api.danafxc.my.id/api/proxy/pict/gambar?q=profilepict&apikey=${dana}`;
+        const response = await axios.get(apiUrl);
+        const jsonData = response.data;
+        if (jsonData && jsonData.status && jsonData.urls && jsonData.urls.length > 0) {
+            const imageUrl = jsonData.urls[0];
+                        conn.sendFile(m.chat, imageUrl, 'random.jpg', `Ini gambar random *${kategori}* untukmu!`, m);
+        } else {
+            throw new Error('API tidak mengembalikan URL gambar yang valid.');
+        }
+
+    } catch (error) {
+        console.error('Error pada fitur random:', error);
+        m.reply('Gagal mengambil gambar. Mungkin sedang ada masalah di server atau kategori ini sedang kosong.');
+    }
+};
+
+handler.help = ["profilepict"];
+handler.tags = ['internet', 'tools'];
+handler.command = /^(profilepict)$/i;
+
+module.exports = handler;
