d87b16a285fd6972170bca3a563ffd446265dde2
danaputra133 <danaputra375@gmail.com>
2026-03-10 17:34:08 +0700

    fix random img

---
+++ plugins/random_wallpaper.js
@@ -0,0 +1,27 @@
+
+const axios = require('axios');
+
+let handler = async (m, { conn }) => {
+    try {
+        m.reply("⏳ Sedang mencari gambar random wallpaper...");
+        const apiUrl = `https://api.danafxc.my.id/api/proxy/pict/gambar?q=profilepict&apikey=${dana}`;
+        const response = await axios.get(apiUrl);
+        const jsonData = response.data;
+        if (jsonData && jsonData.status && jsonData.urls && jsonData.urls.length > 0) {
+            const imageUrl = jsonData.urls[0];
+            await conn.sendFile(m.chat, imageUrl, 'random.jpg', 'Ini gambar random wallpaper untukmu!', m);
+        } else {
+            throw new Error('API tidak mengembalikan URL gambar yang valid.');
+        }
+    } catch (error) {
+        console.error("Error pada fitur random wallpaper:", error);
+        m.reply('Gagal mengambil gambar. Mungkin sedang ada masalah di server atau kategori ini sedang kosong.');
+    }
+};
+
+
+handler.help = ["wallpaper"];
+handler.tags = ['internet', 'tools'];
+handler.command = /^(wallpaper)$/i;
+
+module.exports = handler;
