8dedc9483e0e8db3026f10ffa219b615abe669f2
danaputra-ubuntu <danaputra375@gmail.com>
2026-04-19 11:53:13 +0700

    change uploader wm

--- plugins/toimg.js
+++ plugins/toimg.js
@@ -1,38 +1,38 @@
-const sharp = require('sharp');
+// const sharp = require('sharp');
 
-const handler = async (m, { conn, usedPrefix, command }) => {
-  const notStickerMessage = `Reply sticker dengan command *${usedPrefix + command}*`;
+// const handler = async (m, { conn, usedPrefix, command }) => {
+//   const notStickerMessage = `Reply sticker dengan command *${usedPrefix + command}*`;
 
-  if (!m.quoted) throw notStickerMessage;
+//   if (!m.quoted) throw notStickerMessage;
 
-  const q = m.quoted || m;
-  const mime = q.mimetype || '';
+//   const q = m.quoted || m;
+//   const mime = q.mimetype || '';
 
-  if (!/image\/webp/.test(mime)) throw notStickerMessage;
+//   if (!/image\/webp/.test(mime)) throw notStickerMessage;
 
-  try {
+//   try {
 
-    const media = await q.download();
-    const decodedBuffer = await sharp(media).toFormat('png').toBuffer();
-    if (decodedBuffer.length > 0) {
-      await conn.sendFile(m.chat, decodedBuffer, 'out.png', wm, m);
-    } else {
-      throw `${global.eror}`;
-    }
-  } catch (error) {
-    console.error(error);
-    if (error.message === 'Timeout of 10000ms exceeded') {
-      m.reply('Proses konversi terlalu lama. Silakan coba lagi.');
-    } else {
-      m.reply(global.eror);
-    }
-  }
-};
+//     const media = await q.download();
+//     const decodedBuffer = await sharp(media).toFormat('png').toBuffer();
+//     if (decodedBuffer.length > 0) {
+//       await conn.sendFile(m.chat, decodedBuffer, 'out.png', wm, m);
+//     } else {
+//       throw `${global.eror}`;
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     if (error.message === 'Timeout of 10000ms exceeded') {
+//       m.reply('Proses konversi terlalu lama. Silakan coba lagi.');
+//     } else {
+//       m.reply(global.eror);
+//     }
+//   }
+// };
 
-handler.help = ['toimg'];
-handler.tags = ['sticker'];
-handler.command = ['toimg', 'toimage'];
-handler.limit = true;
-handler.register = false;
+// handler.help = ['toimg'];
+// handler.tags = ['sticker'];
+// handler.command = ['toimg', 'toimage'];
+// handler.limit = true;
+// handler.register = false;
 
-module.exports = handler;
+// module.exports = handler;
